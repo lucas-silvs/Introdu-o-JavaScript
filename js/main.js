a81fb056ccb77555dbb7ea5d1f97a33f6822f10c
Lucas Santos <lucas.parkl@outlook.com>
2021-01-23 11:23:27 -0300

    criação dos itens

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*var nome = 'Lucas da Silva Santos';
+var idade = 20+10
+alert('Bem vindo '+ nome+' sua idade é de '+idade+' anos'); // alerta  do nome com a imagem +10 anos
+console.log(nome)
+console.log(idade + 10)
+var frase = 'Japão é o melhor time do mundo'
+console.log (frase)
+frase.replace('Japão','Brasil')
+*/
+
+var lista = ['maça','pêra','Laranja'];
+console.log(lista[1]);
+lista.push('uva');// add no final da lista
+console.log(lista);
+lista.pop(lista); //remove o ultimo item da lista
+console.log(lista.toString());
